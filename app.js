f5505d7fa9202a193fba17e3769f5c090bc3c8f7
Ahmed Moti <ahmedmoti767@gmail.com>
2025-02-14 16:17:36 +0200

    Runnable file inside your browser

---
+++ app.js
@@ -0,0 +1,23 @@
+document.body.style.fontFamily = "Arial, sans-serif";
+document.body.style.textAlign = "center";
+document.body.style.marginTop = "50px"
+
+const container = document.createElement("div");
+container.id = "app";
+document.body.appendChild(container);
+
+const heading = document.createElement("h1");
+heading.innerText = "Welcome to my Vanilla JS application";
+container.appendChild(heading);
+
+const button = document.createElement("button");
+button.innerText = "Click Here Now";
+button.style.padding = "10px 20px";
+button.style.fontSize = "16px";
+button.style.cursor = "pointer";
+button.onclick = () => {
+  const message = document.createElement("p");
+  message.innerText("Button has been clicked!")
+  container.appendChild(message);
+};
+container.appendChild(button);
