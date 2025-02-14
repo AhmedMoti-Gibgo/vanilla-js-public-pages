bcd3dc3157f11ce217617429e378f1e4b8d4a2d2
Ahmed Moti <ahmedmoti767@gmail.com>
2025-02-14 16:58:40 +0200

    adding a string to update the UI

--- app.js
+++ app.js
@@ -2,29 +2,34 @@
 // document.body.style.textAlign = "center";
 // document.body.style.marginTop = "50px"
 
-
-
 const container = document.createElement("div");
 container.id = "app";
 document.body.appendChild(container);
 
-// container.innerHTML = `
-//   <title>Vanilla JS App</title>
-//   <button onclick="alert('Hello!')">Click Me!</button>
-// `
+container.innerHTML = `
+ <h1> <!-- heading -->
+    Welcome to my Vanilla JS application
+  </h1>
+  <button
+    style="padding: 10px 20px; font-size: 16px; cursor: pointer;"
+    onclick="alert('Button has been clicked!')"
+  > <!-- button -->
+    Click Here Now
+  </button>
+`
 
-const heading = document.createElement("h1");
-heading.innerText = "Welcome to my Vanilla JS application";
-container.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerText = "Welcome to my Vanilla JS application";
+// container.appendChild(heading);
 
-const button = document.createElement("button");
-button.innerText = "Click Here Now";
-button.style.padding = "10px 20px";
-button.style.fontSize = "16px";
-button.style.cursor = "pointer";
-button.onclick = () => {
-  const message = document.createElement("p");
-  message.innerText ="Button has been clicked!";
-  container.appendChild(message);
-};
-container.appendChild(button);
+// const button = document.createElement("button");
+// button.innerText = "Click Here Now";
+// button.style.padding = "10px 20px";
+// button.style.fontSize = "16px";
+// button.style.cursor = "pointer";
+// button.onclick = () => {
+//   const message = document.createElement("p");
+//   message.innerText ="Button has been clicked!";
+//   container.appendChild(message);
+// };
+// container.appendChild(button);
